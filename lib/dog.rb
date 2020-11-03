e46897c18268198c43b4cd347064439f9060a689
nmhummel <natalie.hummel@gmail.com>
2020-11-03 16:35:21 -0600

    commit tree afb349029035eee0155d569fe22a19cc3a13128b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        #stores each new instance of Dog that is instantiated
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        puts @@all.map {|dog| dog.name}
+    end
+    
+    def self.clear_all
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+#binding.pry
+end
